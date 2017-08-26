eff769a0e2e8cabdcaf777002e808d46cf53b22c
Gigi <gislef@gmail.com>
2017-08-26 10:46:15 -0300

    Create theme-options.php

---
+++ theme-options.php
@@ -0,0 +1,41 @@
+
+// REGISTER
+function tema_custom_settings(){   	   
+	  	   
+	    register_setting(
+			'tema-setting-group',//string $option_group
+			'imagens_home' //string $option_name
+			//calback $sanitize_calback		 
+		);
+    
+ }
+ --------
+ 
+ //FIELD
+ 
+ add_settings_field(
+		 'home-imagens-top',//string $id
+		 'Imagens do Slider',//String $title
+		 'tema_home_imgs',//string $calback
+		 'opcoes_do_tema_pagina_inicial',//string $page	
+		 'tema-home-options'//string $section
+		 //string $args			 
+		 );
+     
+ ------
+ 
+ //CALBACK
+ 
+ function tema_home_imgs($urlsparasalvar){	 
+	    $urlsSalvas  = esc_attr( get_option( 'imagens_home' ) );
+	   
+	   	include( get_template_directory() . '/inc/templates/selecao_imagens.php');
+
+	  	if ( isset( $_POST['my_image_URL'] ) ) {
+			$urlsparasalvar  = htmlspecialchars(json_encode($_POST['my_image_URL']));
+			
+		     echo '<input name="imagens_home" value="'.$urlsparasalvar .'" style="width:300px"/>';
+		}	
+ }
+ 
+ 
